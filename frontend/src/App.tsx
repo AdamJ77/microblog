b70419309e73d584b44c928b5b171835cc2cb316
misat1505 <misat2002@gmail.com>
2023-11-08 12:59:08 +0100

    lazy loading of pages with suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import Home from "./pages/Home";
+import React, { lazy, Suspense, ComponentType } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NotFound from "./pages/NotFound";
+import Loading from "./components/common/Loading";
+
+const Home = lazy(() => import("./pages/Home"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 export default function App() {
+  const suspensify = (Component: ComponentType) => {
+    return (
+      <Suspense fallback={<Loading text="App is loading..." />}>
+        <Component />
+      </Suspense>
+    );
+  };
+
   const routes = [
     {
       path: "/",
@@ -14,11 +24,16 @@ export default function App() {
       component: NotFound,
     },
   ];
+
   return (
     <Router>
       <Routes>
         {routes.map((route, index) => (
-          <Route key={index} path={route.path} Component={route.component} />
+          <Route
+            key={index}
+            path={route.path}
+            element={suspensify(route.component)}
+          />
         ))}
       </Routes>
     </Router>
