b0beced576e698cd541e66553620c71190e0e6e2
Maciej Kozłowski <maciejmaciejkoz@gmail.com>
2023-11-09 23:04:02 +0100

    Use setuptools-scm for versioning

--- backend/setup.py
+++ backend/setup.py
@@ -1,6 +1,11 @@
+from pathlib import Path
 from subprocess import check_output
 
 from setuptools import find_packages, setup
+
+
+BACKEND_PATH = Path(__file__).parent
+ROOT = BACKEND_PATH.parent
 
 
 def git_tag() -> str:
@@ -14,11 +19,14 @@ def read_requirements(path) -> list[str]:
 
 setup(
     name='microblog-api',
-    version=git_tag(),
     packages=find_packages(include=['api*']),
     include_package_data=True,
-    install_requires=read_requirements('./requirements.txt'),
-    extras_require={'dev': read_requirements('./requirements.dev.txt')},
+    install_requires=read_requirements(BACKEND_PATH/'requirements.txt'),
+    use_scm_version={
+        "root": ROOT
+    },
+    setup_requires='setuptools_scm',
+    extras_require={'dev': read_requirements(BACKEND_PATH/'requirements.dev.txt')},
     entry_points={
         'console_scripts': [
             'microblog-api=api.__main__:main'
