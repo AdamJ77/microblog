8e149404551fd9c04c7bc8202067e84ae6a31402
Jakub Stacherski <o503@interia.eu>
2023-11-12 16:50:02 +0100

    Small test refactorization

--- backend/tests/domain/conftest.py
+++ backend/tests/domain/conftest.py
@@ -1,30 +1,30 @@
 import pytest
+from domain import entities, gateways
 
 
 @pytest.fixture
 def post_author():
-    from domain import entities
     return entities.User(name='Author')
 
 
 @pytest.fixture
 def post(post_author):
-    from domain.entities import Post
     from datetime import datetime
+
     post_date = datetime.fromtimestamp(0)
-    return Post(text='Bajojajo', author=post_author, datetime=post_date)
+    return entities.Post(
+        text='Bajojajo',
+        author=post_author,
+        datetime=post_date)
 
 
 @pytest.fixture
 def user():
-    from domain import entities
     return entities.User(name='Maciej')
 
 
 @pytest.fixture()
 def repo():
-    from domain import entities, gateways
-
     class FakePostRepo(gateways.PostRepoInterface):
         def __init__(self) -> None:
             self.posts = []
@@ -39,6 +39,4 @@ def repo():
 
 @pytest.fixture
 def timeline():
-    from domain import entities
-
     return entities.Timeline(capacity=1)
