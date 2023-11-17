aa3b7043dbcfff3259a193018a148be9eb174b7e
misat1505 <misat2002@gmail.com>
2023-11-17 17:51:47 +0100

    layout of signup

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { CSSProperties } from "react";
+import SignupForm from "../components/signup/SignupForm";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const wrapperStyles: CSSProperties = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/images/login_background.png)`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    width: "100%",
+    height: "100vh",
+  };
+
+  return (
+    <div style={wrapperStyles}>
+      <SignupForm />
+    </div>
+  );
 }
