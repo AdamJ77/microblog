04776decae002830d4d2341e3f8af422e3555639
Maciej Kozłowski <maciejmaciejkoz@gmail.com>
2023-11-18 02:35:46 +0100

    Add api conftest file

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,15 +1,6 @@
 from pathlib import Path
 
-from fastapi.testclient import TestClient
 import pytest
-
-
-@pytest.fixture
-def client() -> TestClient:
-    from backend.api.app import create_app
-
-    app = create_app()
-    return TestClient(app)
 
 
 @pytest.fixture(autouse=True)
