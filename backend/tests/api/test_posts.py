3dfd48e4ac2c14aa3ec1111abd7e25967281264c
Jakub Stacherski <o503@interia.eu>
2023-12-03 12:41:44 +0100

    Checking response contents individually

--- backend/tests/api/test_posts.py
+++ backend/tests/api/test_posts.py
@@ -1,10 +1,9 @@
 def test_get_posts_no_posts(client):
-    response = client.get("/posts/?start=0&count=1")
+    response = client.get("/posts/?start=0&count=0")
     assert response.status_code == 200
-    assert response.json() == {
-        "links": {
-            "self": "http://microblog.com/posts?start=0&count=1",
-            "next": "http://microblog.com/posts?start=1&count=1",
-        },
-        "data": [],
+    response_content = response.json()
+    assert response_content["links"] == {
+        "self": "http://microblog.com/posts?start=0&count=0",
+        "next": "http://microblog.com/posts?start=0&count=0",
     }
+    assert response_content["data"] == []
