60d1a558f113c383e95d5ec485d52cedf9aca10e
Adam <a.gepard007@gmail.com>
2023-12-03 14:25:05 +0100

    Added k8s creation setup

---
+++ infrastructure/terraform/modules/services/aks/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.78.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+# MODULES
+module "global_setup" {
+  source = "../../../global"
+}
+
+# Create AKS
+resource "azurerm_kubernetes_cluster" "microblog-aks" {
+  name                = "${var.cluster_name}-${module.global_setup.resource_group_name}"
+  location            = module.global_setup.resource_group_location
+  resource_group_name = module.global_setup.resource_group_name
+  dns_prefix          = var.dns_prefix # used to create FQDN for cluster (<dns_prefix>.<region>.azmk8s.io)
+
+  linux_profile {
+    admin_username = var.admin_username
+
+    ssh_key {
+      key_data = file(var.ssh_public_key_path)
+    }
+  }
+
+  default_node_pool {
+    name       = var.default_node_pool
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+    enable_auto_scaling = true
+    min_count = var.node_min_count
+    max_count = var.node_max_count
+  }
+
+  # A way for AKS to interact with Azure APIs, manage Azure resources, and authenticate
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "test"
+  }
+}
+
