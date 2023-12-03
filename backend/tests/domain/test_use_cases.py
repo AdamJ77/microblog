28b551ece9d71b17b952483151198ba8c80d519c
Jakub Stacherski <o503@interia.eu>
2023-12-03 16:29:28 +0100

    get_subset_of_posts use case taking start index

--- backend/tests/domain/test_use_cases.py
+++ backend/tests/domain/test_use_cases.py
@@ -1,16 +1,17 @@
-from backend.domain.entities import Post
+from backend.domain.entities import Post, Timeline
 from backend.domain import use_cases
 from backend.domain.gateways import (
     PostStorageInterface,
-    TimelineStorageInterface
+    TimelineStorageInterface,
 )
 import pytest
 
 
 def test_add_post(
-        post_storage: PostStorageInterface,
-        timeline_storage: TimelineStorageInterface,
-        post: Post):
+    post_storage: PostStorageInterface,
+    timeline_storage: TimelineStorageInterface,
+    post: Post,
+):
     assert len(post_storage.get_any_posts(10)) == 0
     assert len(timeline_storage.read().posts) == 0
 
@@ -21,17 +22,16 @@ def test_add_post(
 
 
 def test_get_subset_of_posts_no_posts(
-        post_storage: PostStorageInterface,
-        timeline_storage: TimelineStorageInterface,
-        post):
+    post_storage: PostStorageInterface,
+    timeline_storage: TimelineStorageInterface,
+):
     result = use_cases.get_subset_of_posts(post_storage, timeline_storage, 10)
     assert len(result) == 0
 
 
 def test_get_subset_of_posts_timeline_only(
-        post_storage,
-        timeline_storage,
-        post):
+    post_storage, timeline_storage, post
+):
     use_cases.add_post(post_storage, timeline_storage, post)
 
     result = use_cases.get_subset_of_posts(None, timeline_storage, 1)
@@ -42,11 +42,33 @@ def test_get_subset_of_posts_timeline_only(
 
 
 def test_get_subset_of_posts_not_enough_in_timeline(
-        post_storage: PostStorageInterface,
-        timeline_storage: TimelineStorageInterface,
-        post):
+    post_storage: PostStorageInterface,
+    timeline_storage: TimelineStorageInterface,
+    post,
+):
     use_cases.add_post(post_storage, timeline_storage, post)
     use_cases.add_post(post_storage, timeline_storage, post)
 
     result = use_cases.get_subset_of_posts(post_storage, timeline_storage, 2)
     assert len(result) == 2
+
+
+def test_get_subset_of_posts_different_start(
+    post_storage: PostStorageInterface,
+    timeline_storage: TimelineStorageInterface,
+    timeline: Timeline,
+    posts,
+):
+    timeline.init_posts([posts["high priority"]])
+    timeline_storage.write(timeline)
+    post_storage.add_post(posts["low priority"])
+
+    result1 = use_cases.get_subset_of_posts(
+        post_storage, timeline_storage, start=0, count=1
+    )
+    result2 = use_cases.get_subset_of_posts(
+        post_storage, timeline_storage, start=1, count=1
+    )
+
+    assert result1[0].text == "high"
+    assert result2[0].text == "low"
