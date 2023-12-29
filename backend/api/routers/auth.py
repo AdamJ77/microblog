1618608b640b8b332387a9d46cc262f63ab10b8d
misat1505 <misat2002@gmail.com>
2023-12-29 16:31:33 +0100

    users are stored in db

--- backend/api/routers/auth.py
+++ backend/api/routers/auth.py
@@ -9,24 +9,6 @@ from dotenv import load_dotenv
 import secrets
 import hashlib
 
-passwords = [
-    "e6c3da5b206634d7f3f3586d747ffdb36b5c675757b380c6a5fe5c570c714349",
-    "1ba3d16e9881959f8c9a9762854f72c6e6321cdd44358a10a4e939033117eab9"
-]
-
-users = [
-    {
-        "id": "d19ffe4b-d2e1-43d9-8679-c8a21309ac22",
-        "login": "login1",
-        "password": passwords[0],
-    },
-    {
-        "id": "048358e2-42bf-4c1b-9569-7301902c11c7",
-        "login": "login2",
-        "password": passwords[1],
-    },
-]
-
 load_dotenv()
 
 SECRET_KEY = os.getenv("JWT_SECRET", None)
@@ -83,18 +65,19 @@ async def login(request: Request):
         raise HTTPException(status_code=400,
                             detail="Login and password are required")
 
-    found_user = None
-    for user in users:
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        if login == user["login"] and hashed_password == user["password"]:
-            found_user = user
-            break
+    db = request.app.database
 
-    if not found_user:
-        raise HTTPException(status_code=401)
+    user = await db.users.find_one({
+        "login": login,
+        "password": hashlib.sha256(password.encode()).hexdigest()
+    })
+
+    if not user:
+        raise HTTPException(status_code=401,
+                            detail="Invalid login or password")
 
     token_data = {
-        "id": found_user["id"]
+        "id": str(user["_id"])
     }
     access_token = create_jwt_token(token_data)
 
@@ -152,3 +135,27 @@ async def logout(request: Request, user_id: str = Depends(get_current_user)):
     response = JSONResponse(content={"message": "Logged out successfully."})
 
     return response
+
+
+@router.post("/signup")
+async def signup(request: Request):
+    data = await request.json()
+
+    login = data.get("login")
+    password = data.get("password")
+    avatar = data.get("avatar")
+
+    db = request.app.database
+
+    if await db.users.find_one({"login": login}):
+        raise HTTPException(status_code=400,
+                            detail="User with given login exists.")
+
+    result = await db.users.insert_one({
+        "login": login,
+        "password": hashlib.sha256(password.encode()).hexdigest(),
+        "avatar": avatar,
+    })
+
+    user_id = str(result.inserted_id)
+    return {"id": user_id}
