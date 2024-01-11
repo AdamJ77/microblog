aa3ca7254d565e806264b9cd1bfda3927eaadd7c
misat1505 <misat2002@gmail.com>
2024-01-11 13:10:57 +0100

    test for login

--- backend/tests/api/test_auth.py
+++ backend/tests/api/test_auth.py
@@ -3,11 +3,31 @@ import hashlib
 import string
 
 
+def signup_user(client):
+    body = {
+        "username": "Greg",
+        "login": ''.join(random.choice(
+            string.ascii_letters + string.digits) for _ in range(100)),
+        "password": "fake_password",
+        "avatar": "http://microblog.com/avatars/Greg.png"
+    }
+    response = client.post("/auth/signup", json=body).json()
+
+    data = {
+        "id": response["id"],
+        "token": response["token"],
+        "login": body["login"],
+        "password": body["password"]
+    }
+
+    return data
+
+
 def test_signup(client):
     body = {
         "username": "Greg",
         "login": ''.join(random.choice(
-            string.ascii_letters + string.digits) for _ in range(10)),
+            string.ascii_letters + string.digits) for _ in range(100)),
         "password": hashlib.sha256("fake_password".encode()).hexdigest(),
         "avatar": "http://microblog.com/avatars/Greg.png"
     }
@@ -20,4 +40,28 @@ def test_signup(client):
 
     # second signup fails because of the same login
     response = client.post("/auth/signup", json=body)
+    assert response.status_code == 400
+
+
+def test_login(client):
+    new_user = signup_user(client)
+    body = {
+        "login": new_user["login"],
+        "password": new_user["password"]
+    }
+    response = client.post("/auth/login", json=body)
+    assert response.status_code == 200
+    assert 'token' in response.json()
+    assert 'token' in response.cookies
+
+
+def test_login_fails(client):
+    body = {
+        "login": "login that completely doesn't exist",
+        "password": "fake password"
+    }
+    response = client.post("/auth/login", json=body)
+    assert response.status_code == 401
+
+    response = client.post("/auth/login", json={})
     assert response.status_code == 400
