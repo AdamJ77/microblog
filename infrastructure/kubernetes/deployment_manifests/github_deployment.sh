bf654084672441e0a57614d081a76f5f0d169638
AdamJ77 <61241656+AdamJ77@users.noreply.github.com>
2024-01-12 00:57:06 +0100

    Update github_deployment.sh

--- infrastructure/kubernetes/deployment_manifests/github_deployment.sh
+++ infrastructure/kubernetes/deployment_manifests/github_deployment.sh
@@ -5,12 +5,3 @@ K8S_PATH=infrastructure/kubernetes/deployment_manifests
 
 kubectl apply -f $K8S_PATH/backend-deployment.yml 
 kubectl apply -f $K8S_PATH/frontend-deployment.yml
-
-<<<<<<< Updated upstream
-
-NGINX_EXTERNAL_IP=$(kubectl get services nginx-service -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
-kubectl set env deployment/frontend-deployment REACT_APP_SERVER_URL="http://${NGINX_EXTERNAL_IP}/api"
-=======
-# NGINX_EXTERNAL_IP=$(kubectl get services nginx-service -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
-# kubectl set env deployment/frontend-deployment REACT_APP_SERVER_URL="http://${NGINX_EXTERNAL_IP}/api"
->>>>>>> Stashed changes
